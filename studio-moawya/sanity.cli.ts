f26ddcb32389b1ead552e24716c8425f7db15b2c
hazemyasserprg <hazemyasser911@gmail.com>
2026-06-20 14:13:10 +0300

    Add Sanity appId to deployment settings

--- studio-moawya/sanity.cli.ts
+++ studio-moawya/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'hy74da6xhoip2c6q1fqjr85s',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
